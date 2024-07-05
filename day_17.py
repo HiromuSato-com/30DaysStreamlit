27672e672d38e27f3e212717e86b731b5db6f809
HiromuSato-com <sato_hiromu@comture.com>
2024-07-05 16:27:06 +0900

    Update day_17.py

--- day_17.py
+++ day_17.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Jul  5 16:13:32 2024
 
@@ -6,6 +5,18 @@ Created on Fri Jul  5 16:13:32 2024
 """
 
 import streamlit as st
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"]) # Hiromu
+st.write("DB password:", st.secrets["db_password"]) # mumuHiro15
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"]) # Dog and Mimi
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 st.title('st.secrets')
 
